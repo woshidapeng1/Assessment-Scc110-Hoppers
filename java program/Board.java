65211b19884e9fcd655c82a7b360e07204aa4fe3
woshidapeng1 <58641406+woshidapeng1@users.noreply.github.com>
2020-03-18 00:16:03 +0000

    Update Board.java

--- java program/Board.java
+++ java program/Board.java
@@ -6,10 +6,9 @@ import javax.swing.*;
 
 public class Board implements ActionListener{
     private static Square[] squArr = new Square[25];
-    Square startSquare, endSquare;
-    static int s1 = -1;
-    static int s2 = -1;
-    int step = 1;
+    private JFrame win = new JFrame("Hoppers");
+    private JOptionPane victoryMessage; 
+    
 
     public Board(){
 
@@ -33,12 +32,20 @@ public class Board implements ActionListener{
                     int row = i / 5;
                     int col = i % 5;
                     String info = "";
-                    info=Square.Water;
-                    if ((row + col) % 2 != 0)
-				squArr[i] = new Square(i, info);
-			else {
-				squArr[i] = new Square(i, Square.LilyPad);
-			}
+                
+                    if ((row + col) % 2 != 0){
+                        squArr[i] = new Square(i, Square.Water);
+                    }else {
+				        squArr[i] = new Square(i, Square.LilyPad);
+            }
+                    if(i==2 || i==10 ){
+                        squArr[i] = new Square(i,Square.GreenFrog);
+                    }
+                    if(i==20){
+                        squArr[i] = new Square(i,Square.RedFrog);
+                    }
+            
+
                     
                     squArr[i].getBtn().addActionListener(this);
                     squArr[i].getBtn().setActionCommand(i + "");
@@ -47,12 +54,153 @@ public class Board implements ActionListener{
                 }
         
             }
+           
             @Override
             public void actionPerformed(ActionEvent e) {
-        
+    
                 int num = Integer.parseInt(e.getActionCommand());
-                int row = num / 5;
-                int col = num % 5;
+            if (squArr[num].getInfo().equals(Square.GreenFrog)) {
+                    squArr[num].setInfo(Square.GreenFrog2);
+                } else if (squArr[num].getInfo().equals(Square.GreenFrog2)) {
+                    squArr[num].setInfo(Square.GreenFrog);
+                }
+        
+                else if (squArr[num].getInfo().equals(Square.LilyPad)) {
+        
+                        if (num-12>=0&&squArr[num - 12].getInfo().equals(Square.GreenFrog2)) {
+                            if (squArr[num - 6].getInfo().equals(Square.GreenFrog)) {
+                                squArr[num - 12].setInfo(Square.LilyPad);
+                                squArr[num].setInfo(Square.GreenFrog);
+                                squArr[num - 6].setInfo(Square.LilyPad);
+        
+                            }
+                        } else if (num+12<=24&&squArr[num + 12].getInfo().equals(Square.GreenFrog2)) {
+                            if (squArr[num + 6].getInfo().equals(Square.GreenFrog)) {
+                                squArr[num + 12].setInfo(Square.LilyPad);
+                                squArr[num].setInfo(Square.GreenFrog);
+                                squArr[num + 6].setInfo(Square.LilyPad);
+        
+                            }
+                        } else if (num-8>=0&&squArr[num - 8].getInfo().equals(Square.GreenFrog2)) {
+                            if (squArr[num - 4].getInfo().equals(Square.GreenFrog)) {
+                                squArr[num - 8].setInfo(Square.LilyPad);
+                                squArr[num].setInfo(Square.GreenFrog);
+                                squArr[num - 4].setInfo(Square.LilyPad);
+        
+                            }
+                        } else if (num+8<=24&&squArr[num + 8].getInfo().equals(Square.GreenFrog2)) {
+                            if (squArr[num + 4].getInfo().equals(Square.GreenFrog)) {
+                                squArr[num + 8].setInfo(Square.LilyPad);
+                                squArr[num].setInfo(Square.GreenFrog);
+                                squArr[num + 4].setInfo(Square.LilyPad);
+        
+                            }        
+                        }else if (num-20>=0&&squArr[num - 20].getInfo().equals(Square.GreenFrog2)) {
+                            if (squArr[num - 10].getInfo().equals(Square.GreenFrog)) {
+                                squArr[num - 20].setInfo(Square.LilyPad);
+                                squArr[num].setInfo(Square.GreenFrog);
+                                squArr[num - 10].setInfo(Square.LilyPad);
+        
+                            }        
+                        }else if (num+20<=24&&squArr[num + 20].getInfo().equals(Square.GreenFrog2)) {
+                            if (squArr[num + 10].getInfo().equals(Square.GreenFrog)) {
+                                squArr[num + 20].setInfo(Square.LilyPad);
+                                squArr[num].setInfo(Square.GreenFrog);
+                                squArr[num + 10].setInfo(Square.LilyPad);
+        
+                            }        
+                        }else if (num-4>=0&&squArr[num - 4].getInfo().equals(Square.GreenFrog2)) {
+                            if (squArr[num - 2].getInfo().equals(Square.GreenFrog)) {
+                                squArr[num - 4].setInfo(Square.LilyPad);
+                                squArr[num].setInfo(Square.GreenFrog);
+                                squArr[num - 2].setInfo(Square.LilyPad);
+        
+                            }        
+                        }else if (num+4<=24&&squArr[num + 4].getInfo().equals(Square.GreenFrog2)) {
+                            if (squArr[num + 2].getInfo().equals(Square.GreenFrog)) {
+                                squArr[num + 4].setInfo(Square.LilyPad);
+                                squArr[num].setInfo(Square.GreenFrog);
+                                squArr[num + 2].setInfo(Square.LilyPad);
+        
+                            }        
+                        }
+                    
+                }
+            if (squArr[num].getInfo().equals(Square.RedFrog)) {
+                squArr[num].setInfo(Square.RedFrog2);
+            } else if (squArr[num].getInfo().equals(Square.RedFrog2)) {
+                squArr[num].setInfo(Square.RedFrog);
             }
     
-}
+            else if (squArr[num].getInfo().equals(Square.LilyPad)) {
+    
+                    if (num-12>=0&&squArr[num - 12].getInfo().equals(Square.RedFrog2)) {
+                        if (squArr[num - 6].getInfo().equals(Square.GreenFrog)) {
+                            squArr[num - 12].setInfo(Square.LilyPad);
+                            squArr[num].setInfo(Square.RedFrog);
+                            squArr[num - 6].setInfo(Square.LilyPad);
+    
+                        }
+                    } else if (num+12<=24&&squArr[num + 12].getInfo().equals(Square.RedFrog2)) {
+                        if (squArr[num + 6].getInfo().equals(Square.GreenFrog)) {
+                            squArr[num + 12].setInfo(Square.LilyPad);
+                            squArr[num].setInfo(Square.RedFrog);
+                            squArr[num + 6].setInfo(Square.LilyPad);
+    
+                        }
+                    } else if (num-8>=0&&squArr[num - 8].getInfo().equals(Square.RedFrog2)) {
+                        if (squArr[num - 4].getInfo().equals(Square.GreenFrog)) {
+                            squArr[num - 8].setInfo(Square.LilyPad);
+                            squArr[num].setInfo(Square.RedFrog);
+                            squArr[num - 4].setInfo(Square.LilyPad);
+    
+                        }
+                    } else if (num+8<=24&&squArr[num + 8].getInfo().equals(Square.RedFrog2)) {
+                        if (squArr[num + 4].getInfo().equals(Square.GreenFrog)) {
+                            squArr[num + 8].setInfo(Square.LilyPad);
+                            squArr[num].setInfo(Square.RedFrog);
+                            squArr[num + 4].setInfo(Square.LilyPad);   
+                        }   
+                    }else if (num-20>=0&&squArr[num - 20].getInfo().equals(Square.RedFrog2)) {
+                        if (squArr[num - 10].getInfo().equals(Square.GreenFrog)) {
+                            squArr[num - 20].setInfo(Square.LilyPad);
+                            squArr[num].setInfo(Square.RedFrog);
+                            squArr[num - 10].setInfo(Square.LilyPad);   
+                        }   
+                    }else if (num+20<=24&&squArr[num + 20].getInfo().equals(Square.RedFrog2)) {
+                        if (squArr[num + 10].getInfo().equals(Square.GreenFrog)) {
+                            squArr[num + 20].setInfo(Square.LilyPad);
+                            squArr[num].setInfo(Square.RedFrog);
+                            squArr[num + 10].setInfo(Square.LilyPad);   
+                        }   
+                    }else if (num-4>=0&&squArr[num - 4].getInfo().equals(Square.RedFrog2)) {
+                        if (squArr[num - 2].getInfo().equals(Square.GreenFrog)) {
+                            squArr[num - 4].setInfo(Square.LilyPad);
+                            squArr[num].setInfo(Square.RedFrog);
+                            squArr[num - 2].setInfo(Square.LilyPad);   
+                        }   
+                    }else if (num+4<=24&&squArr[num + 4].getInfo().equals(Square.RedFrog2)) {
+                        if (squArr[num + 2].getInfo().equals(Square.GreenFrog)) {
+                            squArr[num + 4].setInfo(Square.LilyPad);
+                            squArr[num].setInfo(Square.RedFrog);
+                            squArr[num + 2].setInfo(Square.LilyPad);   
+                        }   
+                    }          
+                }
+            }
+            private void checkWin(){
+                int greenFrog = 0;
+                boolean redFrog = false;
+                for(int i = 0; i < 24; i++){
+                    if(squArr[i].hasfrog()==1){
+                        greenFrog++;
+                    }else if(squArr[i].hasfrog()==2){
+                        redFrog = true;
+                    }
+                }
+                if(greenFrog > 0 || !redFrog){
+                    return;
+                }
+                JOptionPane.showMessageDialog(win, "Winner!", "Result", JOptionPane.INFORMATION_MESSAGE);
+            }
+        }
